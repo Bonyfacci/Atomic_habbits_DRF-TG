586ae8f89b9a1ab6a5faa6b34518079301de48b5
Сергей Платонов <platonovpochta@gmail.com>
2023-11-02 22:47:07 +0100

    Validators add Функции add В сущность add Класс

--- app_habits/models.py
+++ app_habits/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from users.models import User
 
@@ -8,35 +9,43 @@ NULLABLE = {'blank': True, 'null': True}
 class Habit(models.Model):
     """ Сущность привычки """
 
-    # Пользователь — создатель привычки.
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', **NULLABLE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', **NULLABLE,
+                              help_text='Пользователь — создатель привычки.')
 
-    # Место — место, в котором необходимо выполнять привычку.
-    place = models.CharField(max_length=100, verbose_name='Место')
-    # Время — время, когда необходимо выполнять привычку.
-    time = models.TimeField(verbose_name='Время')
-    # Действие — действие, которое, представляет из себя, привычка.
-    action = models.TextField(verbose_name='Действие')
-    # Периодичность (по умолчанию ежедневная) — периодичность выполнения привычки для напоминания в днях.
-    periodicity = models.PositiveIntegerField(default=1, verbose_name='Периодичность выполнения')
-    # Время на выполнение — время, которое предположительно потратит пользователь на выполнение привычки.
-    time_to_complete = models.IntegerField(verbose_name='Время на выполнение, в секундах')
+    place = models.CharField(max_length=100, verbose_name='Место',
+                             help_text='Место — место, в котором необходимо выполнять привычку.')
+    time = models.TimeField(verbose_name='Время',
+                            help_text='Время — время, когда необходимо выполнять привычку.')
+    action = models.TextField(verbose_name='Действие',
+                              help_text='Действие — действие, которое, представляет из себя, привычка.')
+    periodicity = models.PositiveIntegerField(default=1, verbose_name='Периодичность выполнения',
+                                              help_text='Периодичность (по умолчанию ежедневная) — '
+                                                        'периодичность выполнения привычки для напоминания в днях.')
+    time_to_complete = models.IntegerField(verbose_name='Время на выполнение, в секундах',
+                                           help_text='Время на выполнение — время, которое предположительно '
+                                                     'потратит пользователь на выполнение привычки.')
 
-    # Признак приятной привычки — привычка, которую можно привязать к выполнению полезной привычки.
-    pleasant_habit = models.BooleanField(default=False, verbose_name='Признак приятной привычки')
-    # Связанная привычка — привычка, которая связана с другой привычкой,
-    # важно указывать для полезных привычек, но не для приятных.
-    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='Связанная привычка', **NULLABLE)
+    pleasant_habit = models.BooleanField(default=False, verbose_name='Признак приятной привычки',
+                                         help_text='Признак приятной привычки — привычка, '
+                                                   'которую можно привязать к выполнению полезной привычки.')
+    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='Связанная привычка', **NULLABLE,
+                                      help_text='Связанная привычка — привычка, которая связана с другой привычкой, '
+                                                'важно указывать для полезных привычек, но не для приятных.')
 
-    # Вознаграждение — чем пользователь должен себя вознаградить после выполнения.
-    reward = models.TextField(verbose_name='Вознаграждение за выполнение действия', **NULLABLE)
+    reward = models.TextField(verbose_name='Вознаграждение за выполнение действия', **NULLABLE,
+                              help_text='Вознаграждение — чем пользователь должен себя вознаградить после выполнения.')
 
-    # Признак публичности — привычки можно публиковать в общий доступ,
-    # чтобы другие пользователи могли брать в пример чужие привычки.
-    is_public = models.BooleanField(default=False, verbose_name='Признак публичности привычки')
+    is_public = models.BooleanField(default=False, verbose_name='Признак публичности привычки',
+                                    help_text='Признак публичности — привычки можно публиковать в общий доступ, '
+                                              'чтобы другие пользователи могли брать в пример чужие привычки.')
 
     def __str__(self):
         return f'Я буду {self.action} в {self.time} в {self.place}'
+
+    def save(self, *args, **kwargs):
+        """ Проверки выполнения привычки (должно быть не более 120 секунд) """
+        if self.time_to_complete and self.time_to_complete > 120:
+            raise ValidationError('Время выполнения привычки должно быть больше 0 и меньше 120 секунд!')
 
     class Meta:
         verbose_name = 'Привычка'
