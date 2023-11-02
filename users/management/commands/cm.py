ac6358766cf0fa64fd5b03cca6b70b6c80bdbaef
Сергей Платонов <platonovpochta@gmail.com>
2023-11-02 20:58:49 +0100

    users add Model add serializers add views add urls

---
+++ users/management/commands/cm.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        user = User.objects.create(
+            email='manager',
+
+            is_staff=True,
+            is_superuser=False
+        )
+
+        user.set_password('manager')
+        user.save()
