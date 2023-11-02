9dcb01b4fa4e30e27d4da2575368231f9944f38a
Сергей Платонов <platonovpochta@gmail.com>
2023-11-02 21:20:03 +0100

    fix

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -8,6 +8,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         user = User.objects.create(
             username='admin',
+            telegram_username='admin',
+            telegram_chat_id='12345',
 
             is_staff=True,
             is_superuser=True
